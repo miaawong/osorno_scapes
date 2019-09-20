bb5f47322b491115f7819b837787f5a5d33eacd0
Jeff Au <jeffau13@gmail.com>
2019-09-20 19:02:58 -0400

    added rich text support to reviews

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { MARKS } from "@contentful/rich-text-types"
 import RichTextToReact from "rich-text-to-react"
 import styled from "styled-components"
 const ReviewComponent = styled.section`
@@ -9,9 +10,10 @@ const ReviewComponent = styled.section`
   }
 `
 const Review = ({ review }) => {
+  const { json } = review.review
   return (
     <>
-      <ReviewComponent className="col-md-4 col-sm-6 col-xs-12">
+      <ReviewComponent className="col-md-4 col-sm-6 col-xs-12 my-2">
         <div className="clientNameStars">
           <h6 className="px-4">
             stars go here
@@ -19,10 +21,21 @@ const Review = ({ review }) => {
           </h6>
         </div>
         {/* json for review */}
-        {/* <RichTextToReact document={review.review} /> */}
+        <RichTextToReact document={json} options={RichTextOptions} />
       </ReviewComponent>
     </>
   )
+}
+
+const RichTextOptions = {
+  renderMark: {
+    // define styling for BOLD text here. See rich-text-to-react docs for others
+    [MARKS.BOLD]: (text, key) => (
+      <strong key={key} style={{ fontWeight: "bold" }}>
+        {text}
+      </strong>
+    ),
+  },
 }
 
 export default Review
